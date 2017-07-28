cfe6690a4f0aeb99b97675aca900406b21888e69
Ricardo Fideles <ricardo@ricardofideles.com.br>
2017-07-28 10:41:01 -0300

    understagind async await node 7.

---
+++ .history/app-promises_20170728102541.js
@@ -0,0 +1,44 @@
+const users = [{
+    id : 1,
+    name : 'Jhon',
+    schoolId: 101
+},{
+    id : 2,
+    name : 'Jen',
+    schoolId: 999
+}];
+
+const grades = [{
+    id : 1,
+    schoolId : 101,
+    grade : 86
+},{
+    id : 2,
+    schoolId : 999,
+    grade : 100
+},{
+    id : 3,
+    schoolId : 101,
+    grade : 80
+}];
+
+const getUser = (id) => {
+    return new Promise((resolve, reject) => {
+        const user = users.find((user) => user.id === id);
+        if (user) {
+            resolve(user);
+        } else {
+            reject(`Unable to find user with id of ${id}`);
+        }
+    });
+};
+
+const getGrades = () => {
+
+};
+
+getUser(21).then((user) => {
+    console.log(user);
+}).catch((e) => {   
+    console.log(e);
+});
